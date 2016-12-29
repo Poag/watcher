2af1c9ce7721e540703e7e959eaa4d58be791c19
Poag <Poag@users.noreply.github.com>
2016-12-29 11:31:35 +0000

    Delete 30-install.sh

--- etc/init.d/30-install.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/with-contenv bash
-
-# install app 
-[[ ! -d "/opt/plexpy/.git" ]] && \
-	git clone https://github.com/nosmokingbandit/watcher.git /root/watcher
-
-# permissions
-chown -R abc:abc \
-	/root/watcher
