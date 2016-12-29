e8e42abb7b206609e7d5bdc646ad95b71d60cb57
Poag <Poag@users.noreply.github.com>
2016-12-29 11:36:37 +0000

    Update 20-install.sh

--- root/etc/cont-init.d/20-install.sh
+++ root/etc/cont-init.d/20-install.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/with-contenv bash
 
 # install app 
-[[ ! -d "/opt/plexpy/.git" ]] && \
-	git clone https://github.com/nosmokingbandit/watcher.git /watcher
+git clone https://github.com/nosmokingbandit/watcher.git /watcher
 
 # permissions
 chown -R abc:abc \
