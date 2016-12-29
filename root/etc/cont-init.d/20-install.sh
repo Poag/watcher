6a9de2459d7f1a7ab997c1dde501e321db1f5272
Poag <Poag@users.noreply.github.com>
2016-12-29 11:49:11 +0000

    Update 20-install.sh

--- root/etc/cont-init.d/20-install.sh
+++ root/etc/cont-init.d/20-install.sh
@@ -3,6 +3,11 @@
 # install app 
 git clone https://github.com/nosmokingbandit/watcher.git /watcher
 
+# link sqlite to config
+cp -n /watcher/watcher.sqlite /config/watcher.sqlite
+rm /watcher/watcher.sqlite
+ln -s /config/watcher.sqlite /watcher/watcher.sqlite
+
 # permissions
 chown -R abc:abc \
 	/watcher
