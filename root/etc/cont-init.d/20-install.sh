ae62c234da15c63ba7c00512d4622b42392165d8
Poag <Poag@users.noreply.github.com>
2016-12-29 17:17:53 +0000

    Update 20-install.sh
    moved images to config folder

--- root/etc/cont-init.d/20-install.sh
+++ root/etc/cont-init.d/20-install.sh
@@ -8,6 +8,11 @@ cp -n /watcher/watcher.sqlite /config/watcher.sqlite
 rm /watcher/watcher.sqlite
 ln -s /config/watcher.sqlite /watcher/watcher.sqlite
 
+# link images to config folder
+cp -rn /watcher/static/images /config/images
+rm -rf /watcher/static/images
+ln -s /config/images/ /watcher/static/images/
+
 # permissions
 chown -R abc:abc \
 	/watcher
